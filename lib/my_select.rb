307cde8fd2a6075c70ef92200225567ccee6062f
Adam Johnson <adamwjo@gmail.com>
2018-07-25 15:40:47 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,14 @@
+
+
+
 def my_select(collection)
- i = 0
- arr = []
- while i < collection.length
-   yield arr.push(collection[i])
-   if "#{i}".even?
-     i += 1
-   end
- end
- end
+  i = 0
+  arr = []
+  while i < collection.length
+    yield collection[i]
+    if i.even?
+      arr.push(collection[i])
+      i = i + 1
+    end
+  end
+end
