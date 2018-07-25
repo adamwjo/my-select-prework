01b74b8b3437a438b6b27a67d970b781c2c1aae9
Adam Johnson <adamwjo@gmail.com>
2018-07-25 15:19:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,10 +2,8 @@ def my_select(collection)
  i = 0
  arr = []
  while i < collection.length
-   yield collection[i]
+   arr.push yield collection[i]
    if i == collection[i]
-     return i
      i += 1
-   end
  end
 end
